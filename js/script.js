300fbbff1ed0c441b86cda793f3bfd77a811044c
arkhar88 <arkhar88@gmail.com>
2017-04-29 19:28:13 +0200

    table concat

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-var Get_triangle_area = function(base, height) {
-    if (base <= 0 || height <= 0) return 0;
-    return (base * height) / 2;
+// var Get_triangle_area = function(base, height) {
+//     if (base <= 0 || height <= 0) return 0;
+//     return (base * height) / 2;
+// };
+//
+// console.log(Get_triangle_area(10, 4));
+//
+// var a = Get_triangle_area(1, 2);
+// var b = Get_triangle_area(2, 3);
+// var c = Get_triangle_area(3, 4);
+
+var men_tab = ['Arek', 'Marek', 'Jarek', 'Agarek'];
+var women_tab = ['Marta', 'Mercedes', 'Kunegunda', 'Czcibora'];
+
+var concat_tab = men_tab.concat(women_tab);
+
+var add_elem = function(tab, name_string) {
+    if (tab.indexOf(name_string) === -1) return tab.push(name_string);
+    else return 0;
 };
 
-console.log(Get_triangle_area(10, 4));
-
-var a = Get_triangle_area(1, 2);
-var b = Get_triangle_area(2, 3);
-var c = Get_triangle_area(3, 4);
+add_elem(concat_tab, 'Ada');
+console.log(concat_tab);
