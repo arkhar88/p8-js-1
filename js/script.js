b8393d6bbb673d537ef04e1ecfa0eb03dc01dfa8
arkhar88 <arkhar88@gmail.com>
2017-04-30 17:37:12 +0200

    christmas tree in two versions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
-var text ='Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
-var string = 'Zielone słonie';
-string = string.toUpperCase();
-var new_text = text.replace('Papugi', string);
-console.log(new_text.substr(0, new_text.length/2));
+// var tree_generator = function(iter) {
+//     var string = '*';
+//     for (var i = 0; i < iter; i++) {
+//         console.log(string);
+//         string = string.concat('*');
+//     }
+//     return true;
+// };
+//
+// tree_generator(4);
+
+var tree_generator = function(iter) {
+    var string = '';
+    var iter_inside = (2*iter)-1;
+    var left = ((iter_inside - 1) / 2) - 1;
+    var right = ((iter_inside - 1) / 2) + 1;
+
+    for (var i = 0; i < iter; i++) {
+
+        for (var j = 0; j < iter_inside; j++) {
+            if (j <= left) string = string.concat('_');
+            else if (j > left && j < right) string = string.concat('*');
+            else if (j >= right) string = string.concat('_');
+        }
+        console.log(string);
+        string = '';
+        left--;
+        right++;
+    }
+
+    return true;
+};
+
+tree_generator(12);
